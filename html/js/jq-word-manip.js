cd5c6b6f0ac2481de064f862732d0a8786d3974d
Andrew <asotirio@gmail.com>
2017-05-24 15:56:49 -0400

    JS - initial upload of JQ JS file, includes basic writing to dom, p tag creation function, document.ready click event, palindrome checker

--- html/js/jq-word-manip.js
+++ html/js/jq-word-manip.js
@@ -1,7 +1,48 @@
 "use strict";
+var origWord,
+	stripeWord,
+	reversed,
+	myChart,
+	keyArray=[],
+	valueArray=[],
+	$displayOut = $("<div/>").attr("class","phrase");
+
+function displayInfo(){
+	origWord = $('#jq-word').val().toLowerCase();
+	stripeWord = origWord.replace(/[^a-zA-Z]+/g, '');
+	reversed = stripeWord.split("").reverse().join("");
+	$displayOut.append( pElements("The word or phrase you wrote is: ",origWord) );
+	$displayOut.append( pElements("The word or phrase stripped of everything but letters: ",stripeWord) );
+	$displayOut.append( pElements("The length of the string is: ",stripeWord.length) );
+	$displayOut.append( pElements("Is the word or phrase you wrote a palindrome? ", isPala() ) );
+	$displayOut.append( pElements("Amount of times each letter is used: ", "" ) );
+	$displayOut.append( pElements("", "letters broken out" ) );
+	$("#jq-output").append($displayOut);
+}
+
+function pElements(str1, str2){
+	var newpar = $("<p/>").attr("class","jqoutput-text").html(str1 + str2);
+	return newpar;
+}
+
+function isPala(){
+	if(stripeWord === reversed){
+		return "Yes it is."
+	}
+	else{
+		return "No it isn't."
+	}
+}
 
 $(document).ready(function(){
 	$('#jq-submit').click(function(){
-		alert( $('#jq-word').val() );
+		if ( ($('#jq-word').val() != 0) && ($('#jq-word').val().length != 0) ){
+			$("#jq-output .phrase").empty();
+			displayInfo();
+		}
+		else{
+			$$("#jq-output .phrase").empty();
+			$("#jq-output").append("The box above is empty and lonely.");
+		}
 	});
 });
